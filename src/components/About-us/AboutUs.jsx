2ab7e26e0ba568efd2cc19f0ab081aa62f350156
karhin20 <kelvinarhin25@gmail.com>
2024-03-14 17:42:30 +0000

    education site updated

---
+++ src/components/About-us/AboutUs.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./about.css";
+import { Container, Row, Col } from "reactstrap";
+import aboutImg from "../../assests/images/about-us.png";
+import CountUp from "react-countup";
+import "./about.css";
+
+const AboutUs = () => {
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__content">
+              <h2>About Us</h2>
+              <p>
+              Embark on a journey of knowledge and skill development with <strong>Early Rain Tech.</strong>
+              We are a premier online education platform offering a diverse range of courses tailored for kids, 
+              teens, and adults. Whether you're a budding coder, a curious mind, or a seasoned learner, 
+              we have something for everyone.
+              </p>
+
+              <div className="about__counter">
+                <div className=" d-flex gap-5 align-items-center">
+                  <div className="single__counter">
+                    <span className="counter">
+                      <CountUp start={0} end={250} duration={2} />
+                    </span>
+
+                    <p className="counter__title">Enrolled Learners</p>
+                  </div>
+
+                  <div className="single__counter">
+                    <span className="counter">
+                      <CountUp start={0} end={12} duration={2} />
+                    </span>
+
+                    <p className="counter__title">Distinct Courses</p>
+                  </div>
+                </div>
+
+                <div className=" d-flex gap-5 align-items-center">
+                  <div className="single__counter">
+                    <span className="counter">
+                      <CountUp start={0} end={108} duration={2} />
+                    </span>
+
+                    <p className="counter__title">Active Learners</p>
+                  </div>
+
+                  <div className="single__counter">
+                    <span className="counter">
+                      <CountUp start={0} end={5} duration={2} suffix="K" />
+                    </span>
+
+                    <p className="counter__title">Categories Served</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,73 @@
+import React, { useRef } from "react";
+import { Container } from "reactstrap";
+import "./header.css";
+
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
+
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "Pages",
+    url: "#",
+  },
+  {
+    display: "Blog",
+    url: "#",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <i class="ri-pantone-line"></i> Early Rain Tech.
+            </h2>
+          </div>
+
+          <div className="nav d-flex align-items-center gap-5">
+            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li key={index} className="nav__item">
+                    <a href={item.url}>{item.display}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+           < div className="nav__right">
+              <a href="tel:+233543119117" className="mb-0 d-flex align-items-center gap-2">
+                <i className="ri-phone-line"></i> +233 543 119 117
+              </a>
+            </div>
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i class="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
