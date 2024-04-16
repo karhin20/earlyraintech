c9608ce9e54d2565a88eadb843a62b9c6a26d52a
karhin20 <kelvinarhin25@gmail.com>
2024-04-16 17:32:49 +0000

    pwa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React from 'react';
+import { Button } from 'antd';
 import Courses from './pages/Courses';
 import About from './pages/About';
 import CourseDetails from './pages/CourseSinglePage/CourseDetails';
 import Home from "./pages/Home";
 import Register from './components/EnrollSubmit/Enroll'
 import Payments from "./pages/paystacks/Payments"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [installPrompt, setInstallPrompt] = useState(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      e.preventDefault();
+      setInstallPrompt(e);
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (installPrompt) {
+      installPrompt.prompt();
+      installPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        setInstallPrompt(null);
+      });
+    }
+  };
 
   return (
     <BrowserRouter>
@@ -15,12 +44,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/payments" element={<Payments />} />
-        <Route path='/enroll' element={< Register />}/>
+        <Route path='/enroll' element={<Register />}/>
         <Route path="/about" element={<About />} />
         <Route path="/courses/:title" element={<CourseDetails />} />  
       </Routes>
+      <Button style={{ color: 'white', backgroundColor: "#17bf9e" }} type="primary" onClick={handleInstallClick}>Install App</Button>
     </BrowserRouter>
   );   
 }
 
 export default App;
+
