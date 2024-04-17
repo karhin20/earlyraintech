60e1df2544b790d26d63b4791c984ac11752bbf0
karhin20 <kelvinarhin25@gmail.com>
2024-04-17 02:41:19 +0000

    pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,13 @@
+// Service Worker
 import { precacheAndRoute } from 'workbox-precaching';
 
+// Declare self as a global variable
+/* global self */
+
+// Precache and route
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Handle fetch events
 self.addEventListener('fetch', (event) => {
     const { request } = event;
     event.respondWith(
