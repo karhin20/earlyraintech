4655f268bfcf5812442008e7dc8ef5348472318b
karhin20 <kelvinarhin25@gmail.com>
2024-04-17 02:19:03 +0000

    pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,9 +3,10 @@ import { precacheAndRoute } from 'workbox-precaching';
 precacheAndRoute(self.__WB_MANIFEST);
 
 self.addEventListener('fetch', (event) => {
+    const { request } = event;
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.match(request).then((response) => {
+            return response || fetch(request);
         })
     );
 });
